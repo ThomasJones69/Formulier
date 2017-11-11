c1a4408e2310b4119d956315dec853e132e148fc
ThomasJones69 <tjones@ziggo.nl>
2017-11-11 14:34:05 +0100

    Eerste lege opzet formulier

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $naam = "";
+        $adres = "";
+        $postcode = "";
+        $woonplaats = "";
+        $email = "";
+        $geslacht = "";
+        $opmerkingen = "";
+        $website="";
+        
+        // Nu het opzetten van het formulier
+        
+        echo "<h1>Welkom bij de mijn registratie formulier";
+        echo "<br>";
+        echo "<br>";
+        // $naam = $_POST["naam"];
+          
+        ?>
+    </body>
+</html>
